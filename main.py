1c095271cb04d1b53517bd62940a6daca5643bee
Caleb Foster <calebalakaifoster@gmail.com>
2025-07-24 17:39:44 -0700

    added a moving background poorly

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import sys
 import pokemon
 import random
 import json
+from pygame.math import Vector2 as vector
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, path, pos):
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(topleft = pos)
+
+        self.default_pos = vector(self.rect.topleft)
+
+        self.pos = vector(self.rect.topleft)
+        self.speed = 200
+
+    def move(self, dt):
+        if self.pos.x < self.default_pos.x - self.image.get_width():
+            self.pos.x = self.default_pos.x
+        self.pos.x += -1 * self.speed * dt
+        self.rect.x = round(self.pos.x)
+
+    def update(self, dt):
+        self.move(dt)
+
 
 class Game:
     def __init__(self):
@@ -16,6 +37,8 @@ class Game:
 
         self.random = random.randint(0,len(self.pokelist))
         self.pick = self.pokelist[self.random]
+        self.background = Background('wallpaper.jpg', (1280, 0))
+        self.background2 = Background('wallpaper.jpg', (1280 - self.background.image.get_width(), 0))
 
         self.pokemon = pokemon.Pokemon(self.pick['name'])
 
@@ -27,7 +50,14 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            self.display_surface.fill('black')
+            dt = self.clock.tick() / 1000
+
+            self.background.update(dt)
+            self.background2.update(dt)
+
+            self.display_surface.fill('aqua')
+            self.display_surface.blit(self.background.image, self.background.rect.topleft)
+            self.display_surface.blit(self.background2.image, self.background2.rect.topleft)
             self.display_surface.blit(self.pokemon.image, self.pokemon.rect.topleft)
 
             pygame.display.update()
